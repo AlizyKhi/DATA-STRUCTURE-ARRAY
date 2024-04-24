e4f0bde5c0020cc805f2afcd3997b28c1a0e8d54
AlizyKhi <162714208+AlizyKhi@users.noreply.github.com>
2024-04-24 22:13:03 +0500

    Add files via upload
    How To Make ARRAY? (which is a part of data structure)

---
+++ index.ts
@@ -0,0 +1,47 @@
+let students:any []=["ALIZA",'Maheen',"ishrat",1300,70,0.0,true,false]
+console.log(students)
+let firststudentname = students[0]
+console.log(firststudentname)
+console.log(students[1],students[0],students[5],students[7],students[1])
+let Count:number [] =[2000,5000,100]
+console.log(Count)
+let secondCountnumber = Count[1]
+console.log(secondCountnumber) 
+console.log(Count[2],Count[1],Count[0])
+let CountryName:string []=['Austrailia',"Japan",'SaudiaArabia']
+console.log(CountryName)
+let firstCountryName = CountryName[0]
+console.log(firstCountryName)
+let SecondCountryName = CountryName [1]
+console.log(SecondCountryName)
+console.log(CountryName[2],CountryName[0])
+let statement = true;
+let profile = false;
+console.log(profile)
+let GirlName:string [] =["aliza"]
+GirlName =["sadia"]
+console.log(GirlName)
+let BoysName:string []=  ["wajid","subhan"]
+BoysName = ["hamza","farhan"]
+console.log(BoysName) 
+let countryname:string [] = ["FRANCE"]
+countryname = ["mexico"]
+console.log(countryname)
+let bodyname:string [] = ["lip","finger"]
+bodyname = ["teeth","mouth"]
+console.log(bodyname)
+let VegetablesNames:string [] = ["Potato","Onion","Tomato"]
+console.log(VegetablesNames)
+
+
+
+
+
+
+
+
+
+
+
+
+
